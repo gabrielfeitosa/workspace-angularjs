a8f760a008c4b7be80131ae7c946d199ce9290f0
Gabriel Feitosa <gabfeitosa@gmail.com>
2015-03-04 17:10:30 -0300

    mytodo with yo

---
+++ mytodo/app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [ 'Item 1', 'Item 2', 'Item 3'];
+  });
