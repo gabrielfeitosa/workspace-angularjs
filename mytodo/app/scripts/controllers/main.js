6a3fef1ae81b0387c0aaad5ab40ad6a6f6b0215d
Gabriel Feitosa <gabfeitosa@gmail.com>
2015-03-05 14:32:07 -0300

    Final step of yeoman codelab

--- mytodo/app/scripts/controllers/main.js
+++ mytodo/app/scripts/controllers/main.js
@@ -8,6 +8,23 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [ 'Item 1', 'Item 2', 'Item 3'];
+  .controller('MainCtrl', function ($scope,localStorageService) {
+
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
+    $scope.addTodo = function(){
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+    $scope.removeTodo = function(index){
+      $scope.todos.splice(index,1);
+    };
+
+
   });
